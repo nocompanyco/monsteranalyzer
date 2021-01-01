8687f62eda3426506d25791c31e73ccbc0032914
karmelyoei <karmel.salah@gmail.com>
2021-01-01 15:32:29 +0200

    refactor the code make it more simply

--- client/src/components/info-body/info-body.component.jsx
+++ client/src/components/info-body/info-body.component.jsx
@@ -10,60 +10,39 @@ import useStyles from './info-body.styles';
 import DataCollected from '../data-collected/data-collected.component';
 import { ConnectionsInfo, ScannerConfiguration } from './data.js';
 
-export default function InfoBody() {
+export default function InfoBody({ id, header, descrption }) {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
-
+  //simpale collapse panel
+  //datacollected its component to show only the data
   return (
     <div className={classes.root}>
-      <Accordion
-        onChange={handleChange('panel1')}
-        classes={{ root: classes.paper }}
-        defaultExpanded={true}
-      >
+      <Accordion classes={{ root: classes.paper }} defaultExpanded={true}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>Connection Info</Typography>
+          <Typography className={classes.heading}>{header}</Typography>
           <Typography className={classes.secondaryHeading}>
-            Your Network Connection Informations
+            {descrption}
           </Typography>
         </AccordionSummary>
         <AccordionDetails className={classes.summery}>
-          {ConnectionsInfo.map((item, index) => (
-            <DataCollected key={item.id} name={item.name} data={item.data} />
-          ))}
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        onChange={handleChange('panel2')}
-        classes={{
-          root: classes.paper,
-        }}
-        defaultExpanded={true}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography className={classes.heading}>
-            Scanner configuration
-          </Typography>
-          <Typography className={classes.secondaryHeading}>
-            Detialed Scanner confirgration
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className={classes.summery}>
-          {ScannerConfiguration.map((item) => (
-            <DataCollected key={item.id} name={item.name} data={item.data} />
-          ))}
+          {id === 1
+            ? ConnectionsInfo.map((item, index) => (
+                <DataCollected
+                  key={item.id}
+                  name={item.name}
+                  data={item.data}
+                />
+              ))
+            : ScannerConfiguration.map((item) => (
+                <DataCollected
+                  key={item.id}
+                  name={item.name}
+                  data={item.data}
+                />
+              ))}
         </AccordionDetails>
       </Accordion>
     </div>
--- client/src/pages/infoPage/infoPage.component.jsx
+++ client/src/pages/infoPage/infoPage.component.jsx
@@ -1,14 +1,33 @@
-import React, { Fragment ,useState} from 'react';
+import React, { Fragment } from 'react';
 import Header from '../../components/pages-header/pages-header.component';
 import InfoBody from '../../components/info-body/info-body.component';
 
 export default function InfoPage(props) {
   const { history } = props;
- 
+  const data = [
+    {
+      id: 1,
+      header: 'Connection Info',
+      descrption: 'Your Network Connection Informations',
+    },
+    {
+      id: 2,
+      header: 'Scanner configuration ',
+      descrption: 'Detialed Scanner confirgration',
+    },
+  ];
+// infobody which containes the collaspe panel
   return (
     <Fragment>
       <Header history={history} />
-      <InfoBody />
+      {data.map((panel) => (
+        <InfoBody
+          key={panel.id}
+          header={panel.header}
+          descrption={panel.descrption}
+          id={panel.id}
+        />
+      ))}
     </Fragment>
   );
 }
