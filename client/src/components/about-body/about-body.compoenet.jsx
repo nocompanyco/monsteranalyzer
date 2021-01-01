936853bbf7f733ea60d979403fc6a2d694df3bba
karmelyoei <karmel.salah@gmail.com>
2021-01-01 17:19:07 +0200

    building the about page with components #20

---
+++ client/src/components/about-body/about-body.compoenet.jsx
@@ -0,0 +1,36 @@
+import { Typography } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import logo from '../../assets/logo.png';
+import data from './data';
+import DataCollected from '../data-collected/data-collected.component';
+import './about-body.styles.css';
+
+export default function AboutBody() {
+  return (
+    <Fragment>
+      <div className="containerAbout">
+        <div className="section1">
+          <div>
+            <img alt="logo" src={logo} />
+          </div>
+          <div className="textContainer">
+            <Typography className="logoName">Monster Analyzer</Typography>
+            <Typography className="badge">Free Version</Typography>
+          </div>
+        </div>
+
+        <div className="section2">
+
+          {data.map((item) => (
+            <DataCollected
+              key={item.id}
+              name={item.name}
+              data={item.data}
+              page="about"
+            />
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
+}
