17f028d21da1d03c0348e02275cb39112dec0137
karmelyoei <karmel.salah@gmail.com>
2021-01-15 13:23:52 +0200

    state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import './App.css';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
@@ -6,26 +6,22 @@ import LandingPage from './pages/landingPage/landingPage.component.jsx';
 import LanPage from './pages/lanPage/lanPage.component';
 import infoPage from './pages/infoPage/infoPage.component';
 import AboutPage from './pages/aboutPage/aboutPage.component';
-import SettingPage from './pages/settingPage/settingPage.component.jsx'
+import SettingPage from './pages/settingPage/settingPage.component.jsx';
 function App() {
-  const { ipcRenderer } = window.require('electron'); 
-  
+  const { ipcRenderer } = window.require('electron');
+  const [networkOptions, setNetworkOptions] = useState({});
   useEffect(() => {
-    console.log('useEffect')
-    ipcRenderer.sendSync('Selection-NetWork-Setting')
+    console.log('useEffect');
+    ipcRenderer.sendSync('Selection-NetWork-Setting');
     ipcRenderer.on('Selection-NetWork-Setting-Reply', (event, arg) => {
-      console.log('Selection-NetWork-Setting-Reply',arg)
+      console.log('Selection-NetWork-Setting-Reply', arg);
+      setNetworkOptions(
+        { ...networkOptions, net: arg },
+        console.log('put the netowrk at the state', networkOptions)
+      );
     });
-// ipcRenderer.on('Selection-NetWork-Setting', (event, arg) => {
-    //       if (!arg) {
-    //         console.log('didnt get the info');
-    //       }
-    //       console.log('react ipc ',arg)
-    //       ipcRenderer.on('Selection-NetWork-Setting-Reply', (event, arg) => {
-    //         console.log('Selection-NetWork-Setting-Reply',arg)
-    //       });
-    // }); 
-  })
+  });
+
   return (
     <Fragment>
       <Router>
