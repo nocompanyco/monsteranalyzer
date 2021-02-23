f6040d1802a48375d951e51ed1dc760d07319529
karmelyoei <karmel.salah@gmail.com>
2021-02-23 12:59:34 +0200

    changes at host data component

--- client/src/components/host-data/host-data.component.jsx
+++ client/src/components/host-data/host-data.component.jsx
@@ -1,17 +1,44 @@
-import React, { Fragment } from 'react';
-import { Button } from '@material-ui/core';
+import React, { Fragment, useState } from 'react';
+import { Button, FormControlLabel, Checkbox } from '@material-ui/core';
 import './host-data.styles.css';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import CardLan from '../cardLan/cardLan.component';
 
-export default function HostData({ data, onhandleStop, scanStop,handleHostDevice }) {
+export default function HostData({
+  data,
+  onhandleStop,
+  scanStop,
+  handleHostDevice,
+  selectAll,
+  hostSelected,
+  handleselecthost
+}) {
   const matches = useMediaQuery('(max-width:1080px)');
-
+ 
+  console.log('the selection', hostSelected);
   return (
     <Fragment>
       <div className="hostDatacontainer">
         {data.map((device, index) => (
-          <CardLan key={index} hostAddress={device.ip} hostName={device.name} handleHostDevice={handleHostDevice.bind(this,index,device)}/>
+          <div key={device.ip} className="selectContainer">
+            <FormControlLabel
+              key={device.id}
+              control={
+                <Checkbox
+                  name={device.ip}
+                  checked={selectAll ? true : hostSelected[device.ip] || false}
+                  onChange={handleselecthost}
+                />
+              }
+              label=""
+            />
+            <CardLan
+              key={index}
+              hostAddress={device.ip}
+              hostName={device.name}
+              handleHostDevice={handleHostDevice.bind(this, index, device)}
+            />
+          </div>
         ))}
 
         <div className="marginBtn">
